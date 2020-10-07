d73965c13dbdf02973a0691c99583aafe48c793e
Yorick Germs <yorickgerms@MacBook-Pro-van-Yorick-2.local>
2020-10-07 11:10:19 +0200

    move inline js

---
+++ public/js/reverse.js
@@ -0,0 +1,34 @@
+var submitBtn = document.getElementById("task_omdraaien");
+var output = document.getElementById("output");
+
+submitBtn.onclick = function() {reverseInput()};
+function reverseInput(str){
+    var inputValue = document.getElementById("task_input").value;
+    var reversedString = reverseString(inputValue);
+    output.innerText = reversedString;          
+}
+function reverseString(str) {
+    var splitString = emojiStringToArray(str); 
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+    return joinArray;
+}
+function specialChar(s) {
+    return /[^\u0000-\u00ff]/.test(s);
+}
+function emojiStringToArray(str) {
+    split = str.split(/([\uD800-\uDBFF][\uDC00-\uDFFF])/);
+    arr = [];
+    for (var i=0; i<split.length; i++) {
+        char = split[i]
+        if (char !== "") {
+            if(!specialChar(char)){
+                char = char.split("");
+                char = char.reverse();
+                char = char.join("");
+            }
+        arr.push(char);
+        }
+    }
+    return arr;
+};
